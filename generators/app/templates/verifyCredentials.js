7fbaccfffd322f61d0fd05e7c307cac6d8909ac1
Renat Zubairov <renat.zubairov@gmail.com>
2017-09-18 22:24:05 +0200

    Removed unused files

--- generators/app/templates/verifyCredentials.js
+++ generators/app/templates/verifyCredentials.js
@@ -3,8 +3,9 @@
 /**
 * This function will be called by the platform to verify given credentials
 *
+* @returns Promise
 **/
-module.exports = function (credentials, cb) {
+module.exports = function (credentials) {
   console.log('Credentials passed for verification %j', credentials);
   return Promise.resolve(true);
 };
