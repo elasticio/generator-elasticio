c71890a82b333ff0e794096bc87f4d51c95d1657
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-10-10 17:17:42 +0200

    const to let

--- spec/action.spec.js
+++ spec/action.spec.js
@@ -4,7 +4,7 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const fs = require('fs');
 
-let answers = {
+const answers = {
   title: 'My New Action',
   id: 'myTestID',
   mType: 'Static',
