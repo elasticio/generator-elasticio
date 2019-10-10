86c68af2dcb58ebb6d0b91eb9e96a7671292ad4c
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-10-10 10:51:44 +0200

    Refactor all code for 2019

--- generators/action/index.js
+++ generators/action/index.js
@@ -12,36 +12,35 @@ module.exports = class extends Generator {
       this.compDesc = this.fs.readJSON(this.destinationPath('component.json'), {});
       // Have Yeoman greet the user.
       this.log(yosay(
-        'Welcome to the ' + chalk.red('elastic.io action') + ' generator!'
+        `Welcome to the ${chalk.red('elastic.io action')} generator!`,
       ));
       this.log('Loaded component descriptor from %s', this.destinationPath('component.json'));
     } catch (error) {
-      this.log(yosay('I can not find ' + chalk.red('component.json') + ' in the current directory, ' +
-        'please run elasticio:action in the component root folder'));
+      this.log(yosay(`I can not find ${chalk.red('component.json')} in the current directory, `
+        + 'please run elasticio:action in the component root folder'));
       throw error;
     }
   }
-  prompting() {
-    const done = this.async();
 
+  async prompting() {
     const prompts = [{
       type: 'input',
       name: 'title',
       message: 'Please enter an actions title',
-      default: "Upsert Something",
-      validate: function (str) {
+      default: 'Upsert Something',
+      validate(str) {
         return str.length > 0;
-      }
+      },
     }, {
       type: 'input',
       name: 'id',
       message: 'Please enter an action ID',
-      default: function (answers) {
+      default(answers) {
         return _.camelCase(answers.title);
       },
-      validate: function (str) {
+      validate(str) {
         return str.length > 0;
-      }
+      },
     }, {
       type: 'list',
       name: 'mType',
@@ -50,25 +49,21 @@ module.exports = class extends Generator {
         {
           name: 'Static (known at design time)',
           short: 'Static',
-          value: 'Static'
+          value: 'Static',
         },
         {
           name: 'Dynamic (fetched at run time)',
           short: 'Dynamic',
-          value: 'Dynamic'
-        }
-      ]
+          value: 'Dynamic',
+        },
+      ],
     }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
+    this.props = await this.prompt(prompts);
   }
+
   writing() {
-    const id = this.props.id;
+    const { id } = this.props;
     let actions = {};
     if (this.compDesc.actions) {
       actions = this.compDesc.actions;
@@ -77,42 +72,43 @@ module.exports = class extends Generator {
     }
     actions[this.props.id] = {
       title: this.props.title,
-      main: "./lib/actions/" + id + '.js',
-      description: "Description for " + this.props.title
+      main: `./lib/actions/${id}.js`,
+      description: `Description for ${this.props.title}`,
     };
     if (this.props.mType === 'Static') {
       actions[this.props.id].metadata = {
-        in: "./lib/schemas/" + id + ".in.json",
-        out: "./lib/schemas/" + id + ".out.json"
+        in: `./lib/schemas/${id}.in.json`,
+        out: `./lib/schemas/${id}.out.json`,
       };
     } else {
       actions[this.props.id].dynamicMetadata = true;
     }
-
-    this.log('Creating action code file');
-    mkdirp('lib/actions');
-    this.fs.copy(
-      this.templatePath('action' + this.props.mType + '.js'),
-      this.destinationPath('lib/actions/' + id + '.js')
-    );
 
     if (this.props.mType === 'Static') {
       this.log('Creating schema files');
       mkdirp('lib/schemas');
       this.fs.copy(
         this.templatePath('action.in.json'),
-        this.destinationPath('lib/schemas/' + id + '.in.json')
+        this.destinationPath(`lib/schemas/${id}.in.json`),
       );
       this.fs.copy(
         this.templatePath('action.out.json'),
-        this.destinationPath('lib/schemas/' + id + '.out.json')
+        this.destinationPath(`lib/schemas/${id}.out.json`),
       );
     }
+
+    this.log('Creating action file');
+    mkdirp('lib/actions');
+    this.fs.copy(
+      this.templatePath('action.js'),
+      this.destinationPath(`lib/actions/${id}.js`),
+    );
+
     this.log('Creating test');
-    mkdirp('specs');
+    mkdirp('spec');
     this.fs.copy(
       this.templatePath('action.spec.js'),
-      this.destinationPath('spec/' + id + '.spec.js')
+      this.destinationPath(`spec/${id}.spec.js`),
     );
 
     this.fs.writeJSON(this.destinationPath('component.json'), this.compDesc);
--- generators/app/templates/verifyCredentials.js
+++ generators/app/templates/verifyCredentials.js
@@ -1,26 +1,14 @@
-'use strict';
-const co = require('co');
-const rp = require('request-promise');
+/*eslint-disable */
+const Client = require('');
 
-// This function will be called by the platform to verify credentials
-module.exports = function verifyCredentials(credentials, cb) {
-  console.log('Credentials passed for verification %j', credentials);
-
-  co(function*() {
-    console.log('Fetching user information');
-
-    const test = yield rp({
-      uri: 'https://cdn.elastic.io/test.json',
-      json: true
-    });
-
-    console.log('Fetched JSON value=%j', test);
-
-    console.log('Verification completed');
-
-    cb(null, {verified: true});
-  }).catch(err => {
-    console.log('Error occurred', err.stack || err);
-    cb(err , {verified: false});
-  });
+module.exports = async function verify(credentials, callback) {
+  try {
+    const client = new Client(credentials);
+    const requestOptions = {};
+    await client.makeRequest(requestOptions);
+    
+    callback(null, { verified: true });
+  } catch (e) {
+    callback(null, { verified: false });
+  }
 };
